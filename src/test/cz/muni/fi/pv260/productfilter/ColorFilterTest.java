f1965407b88036887aac44839aa568f0abbf7816
Jiri Medved <medved@volny.cz>
2019-04-14 20:36:59 +0200

    ColorFilter Test

---
+++ src/test/cz/muni/fi/pv260/productfilter/ColorFilterTest.java
@@ -0,0 +1,19 @@
+package cz.muni.fi.pv260.productfilter;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ColorFilterTest {
+
+    @Test
+    void testPasses() throws Exception {
+        Filter filter = new ColorFilter(Color.RED);
+
+        assertThat(filter.passes(new Product(1, "Name", Color.RED, BigDecimal.ONE))).isTrue();
+        assertThat(filter.passes(new Product(1, "Name", Color.BLACK, BigDecimal.ONE))).isFalse();
+
+    }
+}
